cbdd953a3b02e981b5978a25436e5d68d503172a
Connor Lawrence <connor.lawrence96@gmail.com>
2021-10-17 17:53:04 -0600

    add save button icon

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,12 @@
 const today = moment().format('LLLL');
       document.getElementById("currentDay").innerHTML = today;
+
+      localStorage.setItem("task","");
+document.getElementById("text").innerHTML = localStorage.getItem("task");
+
+if (typeof(Storage) !== "undefined") {
+      // Store
+      localStorage.setItem("inputText", "");
+      // Retrieve
+      document.getElementById("text").innerHTML = localStorage.getItem("/");
+    }
